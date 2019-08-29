02ac5c7d7966aebff548bce3fc6991acd2b5a036
Ray Eldath <ray.eldath@outlook.com>
2019-08-29 17:26:45 +0800

    (misc|test) JMH test & gradle

--- README.md
+++ README.md
@@ -6,17 +6,15 @@
 
 ### TODO
 
-**Italicized entries indicate that they are WIP now.**
-
  - [X] *Fundamental architecture*
- - [ ] Global scope or option
+ - [X] Global scope or option
    - [X] `required`
    - [X] *`any { }`*
  - [ ] Validation DSL
    - [ ] Number
-   - [ ] *Boolean*
-   - [ ] *String*
-   - [ ] *JSON Object*
+   - [X] Boolean
+   - [ ] String
+   - [ ] JSON Object
    - [ ] JSON Array
    - [ ] `null`
  - [ ] Documentation for API with [Dokka](https://github.com/Kotlin/dokka)
---
+++ src/test/kotlin/ray/eldath/sirius/test/jmh/JmhTest.kt
@@ -0,0 +1,86 @@
+package ray.eldath.sirius.test.jmh
+
+import org.json.JSONObject
+import org.json.JSONTokener
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.results.format.ResultFormatType
+import org.openjdk.jmh.runner.Runner
+import org.openjdk.jmh.runner.options.OptionsBuilder
+import ray.eldath.sirius.api.rootJsonObject
+import ray.eldath.sirius.core.JsonObjectValidationPredicate
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.concurrent.TimeUnit
+
+@BenchmarkMode(Mode.Throughput)
+@Fork(2)
+@Threads(1)
+@Warmup(iterations = 2, time = 8)
+@Measurement(iterations = 4, time = 6)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+open class JmhTest {
+    private val obj: JSONObject
+
+    init {
+        val json = """
+            {
+                "abc": "1234567890",
+                "cde": {
+                    "456": true
+                }
+            }
+        """.trimIndent()
+        obj = (JSONTokener(json)).nextValue() as JSONObject
+    }
+
+    private lateinit var root: JsonObjectValidationPredicate
+
+    @Benchmark
+    @Setup
+    fun build() {
+        root = rootJsonObject {
+            "abc" string {
+                required
+                maxLength = 9
+                length = 1..10
+                test { length in 1..12 }
+            }
+
+            "cde" jsonObject {
+                required
+
+                any {
+                    "123" string {
+                        required
+                    }
+
+                    "456" boolean {
+                        expected = true
+                    }
+                }
+            }
+        }
+    }
+
+    @Benchmark
+    fun test() {
+        root.test(obj)
+    }
+
+    companion object {
+        private const val dic = "build/reports/jmh"
+
+        @JvmStatic
+        fun main(vararg args: String) {
+            Files.createDirectories(Paths.get(dic))
+            val options = OptionsBuilder()
+                .include(JmhTest::class.java.simpleName)
+                .output("$dic/benchmark.txt")
+                .resultFormat(ResultFormatType.JSON)
+                .result("$dic/results.json")
+                .build()
+            Runner(options).run()
+        }
+    }
+}
